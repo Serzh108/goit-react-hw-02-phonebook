c8bdf228c7cd4dc1c37e7ace8b84b1930e0de702
Serzh108 <serzh108@gmail.com>
2020-04-30 14:53:37 +0300

    Home work-02.1 Init version

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>Serzh108 - </code> Test upload HW-02.1.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
