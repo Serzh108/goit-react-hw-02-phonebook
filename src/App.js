0cd4afe2317f0d64bff6be59e28efb8973f10709
Serzh108 <serzh108@gmail.com>
2020-04-30 15:08:23 +0300

    some correction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Serzh108 - </code> Test upload HW-02.1.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>TEST upload GitHub HW-02 part 2</h2>
     </div>
   );
 }
